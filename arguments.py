2f48b60c097997b400ae5bad471f7f8d2a738fc0
gutsssssss <95394016+gutsssssss@users.noreply.github.com>
2024-07-01 15:15:05 +0800

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -8,16 +8,10 @@ num_classes = 10
 batch_size: int = 16
 address1 = 'generated_images/*'
 address2 = 'generated_images2/*'
-# 初始学习率
 initial_lr = 0.00005
-# 衰减因子
 decay_rate = 0.05
-# epoch数
 epochs = 20
-# 正则化参数
 lamda = 0.5
-# dropout率
 dropout_rate = 0.5
-# 初始化参数
 g = 0.5
 normalization = 'L2'
