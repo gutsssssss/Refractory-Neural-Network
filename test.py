62e7da0f90aad4715d356e56124c2ed1c0ec1740
gutsssssss <95394016+gutsssssss@users.noreply.github.com>
2024-08-03 10:26:20 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,4 @@
 import classes as cls
-import torch
 
-n = cls.Neuron(5)
-dataset = []
-for i in range(30):
-    data = torch.randn(5)
-    dataset.append(data)
-    n.forward(data)
-
-print(n.age_list)
-print(n.state)
-
+cls.drawLoop()
 
